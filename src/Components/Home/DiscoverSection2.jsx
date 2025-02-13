283956846e3875b9c9860335659010df6d1e226c
vishnuxprasad <vshnprsdvs+github@gmail.com>
2025-02-13 18:33:50 +0530

    refactor: discover section layout

--- src/Components/Home/DiscoverSection2.jsx
+++ src/Components/Home/DiscoverSection2.jsx
@@ -4,8 +4,8 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "../../assets/SCSS/Home/discover2.scss";
 import data from "../Locale/en-new.json";
-import CartWheel from "../../assets/images/discover/cart-wheel.png";
-import Cloud from "../../assets/images/discover/cloud.png";
+import GiantWheel from "../../assets/images/discover/giant-wheel.svg";
+import YellowCarpet from "../../assets/images/common/yellow-carpet.svg";
 import FireWorks from "../../assets/images/discover/fireworks.png";
 import EnjoySaudi from "../../assets/images/discover/enjoy-saudi-logo.png";
 
@@ -18,40 +18,28 @@ export default function DiscoverSection2() {
       <div className="desc-container-1">
         <p className="section-desc-1">{data.discover.description1}</p>
       </div>
-      <div className="discover-cartwheel-container">
+      <div className="discover-giantwheel-container">
         <div className="container-content">
-          <div className="cartwheel-img">
-            <img src={CartWheel} alt="cart wheel" />
+          <div className="giantwheel-img">
+            <Parallax translateX={[0, 8]} speed={10}>
+              <img src={GiantWheel} alt="cart wheel" />
+            </Parallax>
           </div>
-          <div className="cartwheel-cloud-img-1">
-            <Parallax
-              easing="easeInOutCubic"
-              translateX={[20, 10]}
-              translateY={[0, 0]}
-              speed={10}
-            >
-              <img src={Cloud} alt="cloud" />
+          <div className="giantwheel-carpet">
+            <Parallax speed={15}>
+              <img src={YellowCarpet} alt="carpet" />
             </Parallax>
           </div>
 
-          <div className="cartwheel-fireworks">
+          <div className="giantwheel-fireworks">
             <Parallax
-              translateX={[30, 10]}
-              scale={[0.1, 1]}
-              opacity={[0.3, 1]}
-              speed={30}
+              translateX={[0, 8]}
+              scale={[0.5, 1]}
+              opacity={[0.5, 1]}
+              speed={5}
+              className="fireworks-container"
             >
               <img src={FireWorks} alt="fireworks" />
-            </Parallax>
-          </div>
-          <div className="cartwheel-cloud-img-2">
-            <Parallax
-              easing="easeInOutCubic"
-              translateX={[10, 20]}
-              translateY={[0, 0]}
-              speed={15}
-            >
-              <img src={Cloud} alt="cloud" />
             </Parallax>
           </div>
         </div>
