225f552ca5307ee2da1a09c3530fb2479f33e20e
vishnuxprasad <vshnprsdvs+github@gmail.com>
2025-02-19 13:08:01 +0530

    fix: arabic font in discover section

--- src/Components/Home/DiscoverSection.jsx
+++ src/Components/Home/DiscoverSection.jsx
@@ -3,7 +3,6 @@ import "../../assets/SCSS/Home/discoverSection.scss";
 import { Parallax } from "react-scroll-parallax";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useTranslation } from "react-i18next";
 import { Link, useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import ENV from "../Constants";
@@ -39,7 +38,6 @@ export default function DiscoverSection() {
   const { lang } = useParams();
   const { t } = useTranslation();
   const isMobile = useIsMobile();
-  const { lang } = useParams();
 
   const { trackEvent } = GoogleAnalytics();
   const triggerEvent = (event_label, event_category) => {
@@ -190,12 +188,13 @@ export default function DiscoverSection() {
       </div>
 
       <div className="desc-container-2">
-        <p className="section-desc-2" dir={lang === "ar" ? "rtl" : "ltr"}>{t("discover.description2")}</p>
+        <p className="section-desc-2" dir={lang === "ar" ? "rtl" : "ltr"}>
+          {t("discover.description2")}
+        </p>
         <div className="discover-events-container ">
           <div className="discover-events-button">
             <Link
               className="btn btn-primary"
-              dir={lang === "ar" ? "rtl" : "ltr"}
               dir={lang === "ar" ? "rtl" : "ltr"}
               to={`${ENV.EID_EVENT}${lang}/${ENV.EID_EVENT_QUERY_PARAM}`}
               onClick={triggerEvent("Eid_Events_2024", "link_click")}
