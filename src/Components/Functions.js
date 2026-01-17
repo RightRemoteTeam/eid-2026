ba4ea4cc8eca353a9322bcf242f2dc78eeb42000
vishnuxprasad <vshnprsdvs+github@gmail.com>
2026-01-18 05:14:22 +0530

    feat: simplify navbar, and update HTML lang attribute

--- src/Components/Functions.js
+++ src/Components/Functions.js
@@ -1,22 +1,16 @@
-import React from "react";
-
-export const ToggleThemeButton = ({ theme, toggleTheme }) => {
-  return (
-    <div className={`toggle-button ${theme}`} onClick={toggleTheme}>
-      <img src={""} className="icon" alt="Toggle icon" />
-    </div>
-  );
-};
 export const checkLang = (i18n, lang) => {
   if (lang === "en") {
     i18n.changeLanguage("en");
     document.body.classList.remove("rtl");
+    document.documentElement.setAttribute("lang", "en");
   } else if (lang === "ar") {
     i18n.changeLanguage("ar");
     document.body.classList.add("rtl");
+    document.documentElement.setAttribute("lang", "ar");
   } else {
     i18n.changeLanguage("ar");
     document.body.classList.add("rtl");
+    document.documentElement.setAttribute("lang", "ar");
   }
 };
 export const changeMeta = (lang, title, description) => {
